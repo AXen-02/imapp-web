0c52997e46c01f05625d33866d0f6ce18ff42f40
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-07-19 20:06:36 +0800

    changed logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,12 @@
 import { AccountAvatar } from "./acc-avatar";
 import { ThemeToggler } from "./theme-toggler";
 import { Button } from "./ui/button";
+import {
+  RxLayout,
+  RxHamburgerMenu,
+  RxDiscordLogo,
+  RxComponentBoolean,
+} from "react-icons/rx";
 
 export function Nav() {
   return (
@@ -8,7 +14,8 @@ export function Nav() {
       {/* nav left-side */}
       <div className="mr-4 hidden md:flex">
         <a className="mr-6 flex items-center space-x-2" href="/">
-          <svg
+          <RxComponentBoolean className="h-8 w-8 animate-spin-slow" />
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 256 256"
             className="h-6 w-6"
@@ -36,7 +43,7 @@ export function Nav() {
               stroke-linejoin="round"
               stroke-width="16"
             ></line>
-          </svg>
+          </svg> */}
           <span className="hidden font-bold sm:inline-block">imapp</span>
         </a>
         <nav className="flex items-center space-x-6 text-sm font-medium">
@@ -63,21 +70,7 @@ export function Nav() {
         aria-controls="radix-:R15hja:"
         data-state="closed"
       >
-        <svg
-          width="15"
-          height="15"
-          viewBox="0 0 15 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-        >
-          <path
-            d="M8 2H13.5C13.7761 2 14 2.22386 14 2.5V12.5C14 12.7761 13.7761 13 13.5 13H8V2ZM7 2H1.5C1.22386 2 1 2.22386 1 2.5V12.5C1 12.7761 1.22386 13 1.5 13H7V2ZM0 2.5C0 1.67157 0.671573 1 1.5 1H13.5C14.3284 1 15 1.67157 15 2.5V12.5C15 13.3284 14.3284 14 13.5 14H1.5C0.671573 14 0 13.3284 0 12.5V2.5Z"
-            fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <RxHamburgerMenu width="15" height="15" className="h-6 w-6" />
         <span className="sr-only">Toggle Menu</span>
       </button>
       {/* nav right-side */}
