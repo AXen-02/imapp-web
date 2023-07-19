790ae84f54102e060c1a5b887ca237996f675e65
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-07-19 20:19:04 +0800

    added theme animation

--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -20,8 +20,8 @@ export function ThemeToggler() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 animate-pulse" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 animate-pulse" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
